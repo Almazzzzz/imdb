2a791dd178845e89dffe0003b5f78c409a652cf7
valdemarua <valdemarua@gmail.com>
2016-12-03 02:14:50 +0200

    Add parameter chart type for Series top 250.

--- lib/imdb/series_top_250.rb
+++ lib/imdb/series_top_250.rb
@@ -1,9 +1,14 @@
 module Imdb
   class SeriesTop250 < MovieList
+    
+    def initialize(chart_type = "top250")
+      @chart_type = (chart_type == 'top250') ? 'toptv' : 'tvmeter'
+    end
+    
     private
 
     def document
-      @document ||= Nokogiri::HTML(open('http://akas.imdb.com/chart/toptv'))
+      @document ||= Nokogiri::HTML(open("http://akas.imdb.com/chart/#{chart_type}"))
     end
   end # Top250
 end # Imdb
