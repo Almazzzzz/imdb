ef7597391ce422d49f704e46c01aa5316c566e0b
valdemarua <valdemarua@gmail.com>
2017-03-26 15:33:02 +0300

    Add budget.

--- lib/imdb/base.rb
+++ lib/imdb/base.rb
@@ -304,57 +304,65 @@ module Imdb
       # main_document.xpath("//[@itemprop]")
     end
 
+    def budget
+      currency_to_number(main_document.at("h4[text()*='Budget:']").parent.content.strip.match(/\$[0-9,]*/)) rescue nil
+    end
+
     private
 
-    # Returns a new Nokogiri document for parsing.
-    def document
-      @document ||= Nokogiri::HTML(Imdb::Movie.find_by_id(@id))
-    end
+      # Returns a new Nokogiri document for parsing.
+      def document
+        @document ||= Nokogiri::HTML(Imdb::Movie.find_by_id(@id))
+      end
 
-    def locations_document
-      @locations_document ||= Nokogiri::HTML(Imdb::Movie.find_by_id(@id, 'locations'))
-    end
+      def locations_document
+        @locations_document ||= Nokogiri::HTML(Imdb::Movie.find_by_id(@id, 'locations'))
+      end
 
-    def releaseinfo_document
-      @releaseinfo_document ||= Nokogiri::HTML(Imdb::Movie.find_by_id(@id, 'releaseinfo'))
-    end
+      def releaseinfo_document
+        @releaseinfo_document ||= Nokogiri::HTML(Imdb::Movie.find_by_id(@id, 'releaseinfo'))
+      end
 
-    def fullcredits_document
-      @fullcredits_document ||= Nokogiri::HTML(Imdb::Movie.find_by_id(@id, 'fullcredits'))
-    end
-    
-    def criticreviews_document
-      @criticreviews_document ||= Nokogiri::HTML(Imdb::Movie.find_by_id(@id, 'criticreviews'))
-    end
+      def fullcredits_document
+        @fullcredits_document ||= Nokogiri::HTML(Imdb::Movie.find_by_id(@id, 'fullcredits'))
+      end
+      
+      def criticreviews_document
+        @criticreviews_document ||= Nokogiri::HTML(Imdb::Movie.find_by_id(@id, 'criticreviews'))
+      end
 
-    def main_document
-      @main_document ||= sleep(2) && Nokogiri::HTML(Imdb::Movie.find_by_id(@id, ''))
-    end
-    
-    # Use HTTParty to fetch the raw HTML for this movie.
-    def self.find_by_id(imdb_id, page = :combined)
-      open("http://akas.imdb.com/title/tt#{imdb_id}/#{page}", "Accept-Language" => "en")
-    end
+      def main_document
+        @main_document ||= sleep(1) && Nokogiri::HTML(Imdb::Movie.find_by_id(@id, ''))
+      end
+      
+      # Use HTTParty to fetch the raw HTML for this movie.
+      def self.find_by_id(imdb_id, page = :combined)
+        open("http://akas.imdb.com/title/tt#{imdb_id}/#{page}", "Accept-Language" => "en")
+      end
 
-    # Convenience method for search
-    def self.search(query)
-      Imdb::Search.new(query).movies
-    end
+      # Convenience method for search
+      def self.search(query)
+        Imdb::Search.new(query).movies
+      end
 
-    def self.top_250
-      Imdb::Top250.new.movies
-    end
+      def self.top_250
+        Imdb::Top250.new.movies
+      end
 
-    def sanitize_plot(the_plot)
-      the_plot = the_plot.gsub(/add\ssummary|full\ssummary/i, '')
-      the_plot = the_plot.gsub(/add\ssynopsis|full\ssynopsis/i, '')
-      the_plot = the_plot.gsub(/see|more|\u00BB|\u00A0/i, '')
-      the_plot = the_plot.gsub(/\|/i, '')
-      the_plot.strip
-    end
+      def sanitize_plot(the_plot)
+        the_plot = the_plot.gsub(/add\ssummary|full\ssummary/i, '')
+        the_plot = the_plot.gsub(/add\ssynopsis|full\ssynopsis/i, '')
+        the_plot = the_plot.gsub(/see|more|\u00BB|\u00A0/i, '')
+        the_plot = the_plot.gsub(/\|/i, '')
+        the_plot.strip
+      end
 
-    def sanitize_release_date(the_release_date)
-      the_release_date.gsub(/see|more|\u00BB|\u00A0/i, '').strip
-    end
+      def sanitize_release_date(the_release_date)
+        the_release_date.gsub(/see|more|\u00BB|\u00A0/i, '').strip
+      end
+
+      def currency_to_number(currency)
+        currency.to_s.gsub(/[$,]/,'').to_f
+      end
   end # Movie
 end # Imdb
